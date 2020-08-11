d2d6bf8471182a699cb62ce804232067f1d97180
Tanu7899 <tanmayadash97@gmail.com>
2020-08-12 01:23:24 +0530

    Added Account Statement & Optimised som other code

--- src/main/java/bank/AccountHolderDetails.java
+++ src/main/java/bank/AccountHolderDetails.java
@@ -2,12 +2,15 @@ package bank;
 
 import java.sql.*;
 
-public class AccountHolderDetails extends DBConDetails{
+public class AccountHolderDetails extends SessionUtilities{
+    DBConDetails dbConDetails;
+    public AccountHolderDetails() {
+        dbConDetails=new DBConDetails();
+    }
     //Display all the users details
     public void showAllAccountHolderDetails(){
            try {
-               initialiseMySQLStatement();
-               ResultSet rs = statement.executeQuery("select * from user_details");
+               ResultSet rs = dbConDetails.getStatement().executeQuery("select * from user_details");
                while (rs.next())
                System.out.printf("%-5d %-20s %-15d %-20s %-10d %-30s %-30s \n",rs.getInt("account_id"),
                        rs.getString("name"),
@@ -17,26 +20,19 @@ public class AccountHolderDetails extends DBConDetails{
                        rs.getTimestamp("created_at"),
                        rs.getTimestamp("updated_at"));
            } catch (Exception e) {
-               System.out.println(e);
-           }
-           finally {
-               try {con.close(); }catch (Exception e){e.printStackTrace();}
+               e.printStackTrace();
            }
         }
     //Insert new user details
     public int insertAccountHolderDetails(String name, Long ph_no, String address){
         int insertedInt=0;
         try {
-            initialiseMySQLStatement();
-            insertedInt = statement.executeUpdate("insert into user_details (name,ph_no,address) " +
+            insertedInt = dbConDetails.getStatement().executeUpdate("insert into user_details (name,ph_no,address) " +
                     "values('"+name+"',"+ph_no+",'"+address+"')");
             if (insertedInt>0)
                 System.out.println("Your data inserted successfully");
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            try {con.close(); }catch (Exception e){e.printStackTrace();}
         }
         return insertedInt;
     }
@@ -44,24 +40,24 @@ public class AccountHolderDetails extends DBConDetails{
     public int updateAccountHolderDetails(int account_id, String userDetailsKey,String userDetailsValue){
             int updatedInt=0;
         try {
-            initialiseMySQLStatement();
             if (userDetailsKey.equalsIgnoreCase("name")) {
-                updatedInt = statement.executeUpdate("UPDATE user_details SET name='" + userDetailsValue +
+                updatedInt = dbConDetails.getStatement().executeUpdate("UPDATE user_details SET name='" + userDetailsValue +
                         "' WHERE account_id=" + account_id);
             } else if (userDetailsKey.equalsIgnoreCase("ph_no")) {
                 int userDetailsValueInt=Integer.parseInt(userDetailsValue);
-                updatedInt = statement.executeUpdate("UPDATE user_details SET ph_no='" + userDetailsValueInt +
+                updatedInt = dbConDetails.getStatement().executeUpdate("UPDATE user_details SET ph_no='" + userDetailsValueInt +
                         "' WHERE account_id=" + account_id);
             } else if (userDetailsKey.equalsIgnoreCase("address")) {
-                updatedInt = statement.executeUpdate("UPDATE user_details SET address='" + userDetailsValue +
+                updatedInt = dbConDetails.getStatement().executeUpdate("UPDATE user_details SET address='" + userDetailsValue +
                         "' WHERE account_id=" + account_id);
             }
         } catch (Exception e) {
-            System.out.println(e);
-        }
-        finally {
-            try {con.close(); }catch (Exception e){e.printStackTrace();}
+            e.printStackTrace();
         }
         return updatedInt;
+    }
+
+    public int updateAccountHolderDetails( String userDetailsKey,String userDetailsValue){
+       return updateAccountHolderDetails(session_id,userDetailsKey,userDetailsValue);
     }
 }
--- src/main/java/bank/CredDetails.java
+++ src/main/java/bank/CredDetails.java
@@ -2,20 +2,20 @@ package bank;
 
 import java.sql.ResultSet;
 
-public class CredDetails extends DBConDetails{
+public class CredDetails extends SessionUtilities{
+    DBConDetails dbConDetails;
+    public CredDetails() {
+        dbConDetails = new DBConDetails();
+    }
     //Validate username, password and returns account_id
     public int validateCredentials(String username, String password){
            int accountId=-1;
             try {
-               initialiseMySQLStatement();
-               ResultSet rs = statement.executeQuery("select account_id from cred_details WHERE username ='"+username+"' AND password = '"+password+"'");
+               ResultSet rs = dbConDetails.getStatement().executeQuery("select account_id from cred_details WHERE username ='"+username+"' AND password = '"+password+"'");
                while (rs.next())
              accountId = rs.getInt("account_id");
            } catch (Exception e) {
-               System.out.println(e);
-           }
-           finally {
-               try {con.close(); }catch (Exception e){e.printStackTrace();}
+               e.printStackTrace();
            }
             return  accountId;
         }
@@ -23,31 +23,23 @@ public class CredDetails extends DBConDetails{
     public int insertCredentials(int account_id, String username , String password){
         int insertedInt=0;
         try {
-            initialiseMySQLStatement();
-            insertedInt = statement.executeUpdate("insert into cred_details (account_id,username,password) " +
+            insertedInt = dbConDetails.getStatement().executeUpdate("insert into cred_details (account_id,username,password) " +
                     "values("+account_id+",'"+username+"','"+password+"')");
             if (insertedInt>0)
                 System.out.println("Your data inserted successfully");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        finally {
-            try {con.close(); }catch (Exception e){e.printStackTrace();}
-        }
         return insertedInt;
     }
     //Update only password in credentials for existing user
-    public int updateCredentials(int account_id, String password){
+    public int updateCredentials( String password){
             int updatedInt=0;
         try {
-            initialiseMySQLStatement();
-                updatedInt = statement.executeUpdate("UPDATE cred_details SET password='" + password +
-                        "' WHERE account_id=" + account_id);
+                updatedInt =  dbConDetails.getStatement().executeUpdate("UPDATE cred_details SET password='" + password +
+                        "' WHERE account_id=" + session_id);
         } catch (Exception e) {
-            System.out.println(e);
-        }
-        finally {
-            try {con.close(); }catch (Exception e){e.printStackTrace();}
+            e.printStackTrace();
         }
         return updatedInt;
     }
